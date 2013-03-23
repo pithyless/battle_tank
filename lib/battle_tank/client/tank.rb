e02143c57c8e0ca5a82295800d964fdf2d353d41
Pawel Niemczyk <pawel.niemczyk@o2.pl>
2013-03-23 15:16:47 +0100

    rebuild tank model to use config_loader

--- lib/battle_tank/client/tank.rb
+++ lib/battle_tank/client/tank.rb
@@ -1,43 +1,29 @@
+require 'battle_tank/config_loader'
+
 module BattleTank
   class Client
     class Tank
       def initialize(name, model)
-        @model = model
+        @definition = BattleTank::ConfigLoader.new.tank(model)
         @name = name
-        build_model
         direction(:up)
       end
 
       def show
-        send("#{side.to_s}_side")
+        definition['model']["#{side.to_s}"]
       end
 
       def direction(side)
         @side = side
       end
 
+      def bullet
+        definition['bullet']
+      end
+
       private
 
-      attr_reader :model, :name, :right_side, :left_side, :up_side, :down_side, :bullet_char, :additional_weapon_char, :side
-
-      def map_file
-        File.join(File.dirname(__FILE__), '..', '..', '..', 'models', 'tanks', "#{model}.map")
-      end
-
-      def build_model
-        file = File.open(map_file)
-        map = []
-        file.each_line do |line|
-          map << line.delete("\n")
-        end
-
-        @right_side = map[0..2]
-        @left_side = map[3..5]
-        @up_side = map[6..8]
-        @down_side = map[9..11]
-        @bullet_char = map[12]
-        @additional_weapon_char = map[13]
-      end
+      attr_reader :definition, :name, :side
     end
   end
 end
