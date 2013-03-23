0c49604cf305289daaf92d7bf66dcb5763eba3e9
Adrian Dylinski <gotenhan@gmail.com>
2013-03-23 19:31:14 +0100

    Add battle_tank starter

---
+++ bin/battle_tank.rb
@@ -0,0 +1,6 @@
+require "battle_tank/version"
+require "battle_tank/main_menu_state"
+
+BattleTank::MainMenuState.new.start
+
+
--- lib/battle_tank.rb
+++
@@ -1,5 +0,0 @@
-require "battle_tank/version"
-require "battle_tank/main_menu_state"
-
-BattleTank::MainMenuState.new.start
-
