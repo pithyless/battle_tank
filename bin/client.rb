6d08d9392c7ac10c9bd4fec20186f1de12a4182e
Pawel Niemczyk <pawel.niemczyk@o2.pl>
2013-03-23 15:18:28 +0100

    add some lines to show how to use tank model

--- bin/client.rb
+++ bin/client.rb
@@ -1,12 +1,15 @@
 require 'battle_tank/world'
 require 'battle_tank/view_port'
 require 'battle_tank/control'
+require 'battle_tank/client/tank'
 
 world = World.new(500, 500)
 view = ViewPort.new(world, 100, 100)
 control = Control.new(view)
 
-world.add(5, 10, 'hello')
+t = BattleTank::Client::Tank.new('jojo', "cannon")
+t.direction(:left)
+world.add(5, 10, t.show)
 world.add(1, 5, 'goodbye')
 
 control.view_loop
